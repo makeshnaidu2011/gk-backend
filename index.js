4b5f3823d23731d75a4f74e5a9c32c8321895ce7
mageshnaidu2011 <mageshnaidu2011@gmail.com>
2020-08-18 16:59:25 +0530

    ejs  and mail server integration

--- index.js
+++ index.js
@@ -3,35 +3,58 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require('dotenv').config()
-
+const ejs = require('ejs');
+const nodemailer = require('nodemailer');
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
-
 
 const dataSchema = new mongoose.Schema({
     name: String,
     email: String,
     subject: String
 })
+
 const formQuery = new mongoose.model("formQuery", dataSchema);
-
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const formData = new formQuery({
-    name: "username",
-    email: "test@gmail.com",
-    subject: "It works"
+app.set("view engine", "ejs")
+var transporter = nodemailer.createTransport({
+    service: process.env.SERVICE,
+    auth: {
+        user: process.env.FROM_MAIL,
+        pass: process.env.PASS
+    }
 });
-formData.save();
 
-// app.post("/gkprepress", (req, res) => {
+app.post("/gkprepress", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const subject = req.body.subject;
+    const formData = new formQuery({
+        name: name,
+        email: email,
+        subject: subject
+    });
+
+    var mailOptions = {
+        from: process.env.FROM_MAIL,
+        to: process.env.TO_MAIL,
+        subject: "New form submission",
+        text: name + "    " + email + "     " + subject
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    formData.save().then(() => res.send('submitted'));;
 
 
-// });
-app.get("/", (req, res) => res.send("yo"))
+});
+app.get("/", (req, res) => res.render("index"))
 
 app.listen(PORT, function () {
     console.log("started");
